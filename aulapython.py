38c14e37f59f1246d24b9adcd112af4252b0c914
taeltico <rafaelwvs16@gmaail.com>
2022-04-27 17:32:18 -0300

    aprendendo comentarios e aspas

--- aulapython.py
+++ aulapython.py
@@ -1,2 +1,8 @@
 age = 30
 print(age)
+
+30
+<class int(object):
+    def __init__(self, *args):
+        super(int, self).__init__(*args)
+        
---
+++ main.py
@@ -0,0 +1,11 @@
+#comentario
+print('linha 1')  #comentario
+print('linha 2')  #outro comenterio
+#falando do que se trata essa linha
+#outra linha
+print('linha 3')
+print('linha 4')  # isso é um comentario
+print('linha 5')  # mais um
+print('linha 6')  # Isso mostra linha 6 na tela
+print('qualquer coisa')
+"""" turdo que eu abro com  aspas duplas eu fecho com aspas duplas"""
