58767181ec0b286e2aba5d3e648e93b0b6cea906
taeltico <rafaelwvs16@gmaail.com>
2022-04-27 17:58:32 -0300

    aula de print

---
+++ print.py
@@ -0,0 +1,9 @@
+"""print(123456)
+
+152.242.122-16
+
+print('Rafael', 'Wanderley','Vitor', 'Sá', sep='-', end='######')
+print('Rafael' ,'Wanderley', sep='-', end='')
+"""
+print('152', '242', '122',sep='.', end='-')
+print('16')
